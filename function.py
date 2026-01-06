1e604fed58dbf0136056eb41c09b4720fb6c3870
RHENAHYEAR <graciousm71@gmail.com>
2026-01-06 18:21:12 +0200

    Add files via upload

---
+++ function.py
@@ -0,0 +1,26 @@
+...
+def greet(name):
+    print(f"hello,{name}")
+    
+    greet("alice")
+    
+    def add(a, b):
+        return a + b
+    
+    result = add(2, 5)
+    print(result) 
+...
+  
+def factorial(n):
+      if n == 0:
+          return 1
+      else:
+          return n*factorial(n-1)
+      
+def greet(name, greeting="hello"):
+        print(f"{greeting}, {name}")
+        
+        
+greet("bob")
+        
+        
---
+++ modules.py
@@ -0,0 +1,5 @@
+import math_operation
+
+print(math_operation.add(5, 2))
+
+print(math_operation.subtract(5, 2))
